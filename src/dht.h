ad1028f0ac4d468cf5681546872904aaea4a3bc1
IanRietveld <74311741+IanRietveld@users.noreply.github.com>
2021-06-29 12:08:37 +0200

    Update dht.h

--- src/dht.h
+++ src/dht.h
@@ -1,33 +1,36 @@
 #include "hwlib.hpp"
 /// @file 
 
+
 class dht11
 {
 private:
 
     hwlib::pin_oc& pin;
+	bool temp_fahrenheit = false; 
 
 public:
 								// Public variables to store specific sensor data.
     int humidity;				
-    int temperature;			
+    int temperature_f;
+	int temperature_c;			
 								/// \brief
 								/// Constructor that creates an DHT11 instance, from an open collector pin.
 								
     dht11(hwlib::pin_oc& pin)
         : pin(pin)
     {
-	pin.write(1); 						// Datasheet states that we need to send a start signal. 
+		pin.write(1); 			// Datasheet states that we need to send a start signal. 
     }
 								/// \brief
-    						 		/// Function to read one byte from DHT11.
+    						 	/// Function to read one byte from DHT11.
 								/// \details
 								/// This functions purpose is to read one byte from the sensor
-								/// and saving it into the result variabel, details are described in commentary.
+								/// and saving it into the result variabel.
 	uint8_t read_dht11_byte() 
     {
 	
-	uint8_t result = 0;					// Variable for saving byte of data.
+	uint8_t result = 0;			// Variable for saving byte of data.
 
 	for(unsigned int i = 0; i < 8; i++) {
 
@@ -38,7 +41,7 @@ public:
 
 	    result <<= 1; 			
  
-	    if(pin.read())		// If pin is still '1' after 30us.
+	    if(pin.read())			// If pin is still '1' after 30us.
 
 			result |= 1; 		
 
@@ -46,14 +49,18 @@ public:
 	}
 	return result; 				
     }
+
+
 						/// \brief 
 						///	Read five bytes send by the sensor.
 						/// \details 
-    						/// Function to read the 5 bytes sent by the sensor of which the first (integral RH data) and 
+    					/// Function to read the 5 bytes sent by the sensor of which the first (integral RH data) and 
 						/// third (integral T data) byte contain data which are stored for later usage.  
-    						/// If the high pulse is ~28 microseconds then it's a 0 and if it's ~70 microseconds
-    						/// then it's a 1. (This can be visualized by using a logic analyzer.)
+    					/// If the high pulse is ~28 microseconds then it's a 0 and if it's ~70 microseconds
+    					/// then it's a 1. (This can be visualized by using a logic analyzer.)
 	void get_data(){
+	
+	int temperature; 
 
 	pin.write(0); 		
 	hwlib::wait_ms(25);
@@ -62,10 +69,10 @@ public:
 	hwlib::wait_us(60);
 
 						// First expect a low signal for ~80 microseconds followed by a high signal
-    						// for ~80 microseconds again.
+    					// for ~80 microseconds again.
 
 	if(pin.read()) {
-	    					// Wait for first response: Low.
+	    				// Wait for first response: Low.
 	    return;
 	}
 	hwlib::wait_us(80);
@@ -75,16 +82,49 @@ public:
 	    return;
 	}
 
-	hwlib::wait_us(80); 			// After 80us wait, ready for data reception.
+	hwlib::wait_us(80); // After 80us wait, ready for data reception.
 
 	for(unsigned int i = 0; i < 5; i++)
 	{
+
 		if (i == 0)
 			humidity = read_dht11_byte(); 		// Store first byte in variabel "humidity"
 		else if (i == 2)
-			temperature = read_dht11_byte();	// Store first byte in variabel "Temperature"	
+			{
+				temperature = read_dht11_byte();  				// Store third byte in variabel "Temperature_c"	
+				// temperature_f = (read_dht11_byte() * 1.8 + 32);  	// Store third byte in variabel "Temperature_f"	
+			}
+			 		
 		else
 			read_dht11_byte();
 	}
+	temperature_c = temperature; 
+	temperature_f = temperature * 1.8 + 32;
+  }
+
+
+						/// \brief 
+						/// function to get temperature in either fahrenheit or celcius.
+  int get_temperature()		
+  {
+	if (temp_fahrenheit)
+		return temperature_f;
+
+	return temperature_c; 
+  }
+						/// \brief
+						/// function to set string to right scale.
+  const char* get_temperature_scale()
+  {
+	  if (temp_fahrenheit)
+	  	return " Fahrenheit";
+	  return " Celcius";
+  }	
+						/// \brief
+						/// boolean function to set wether u want to use fahrenheit or celcius.
+  void set_fahrenheit(bool F)
+  {
+	  temp_fahrenheit = F; 
   }
 };
+
